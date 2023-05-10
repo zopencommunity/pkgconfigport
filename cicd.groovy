a818a2f986d1e17c6b9ecd4b48d608119dc86441
Igor Todorovski <itodorov@ca.ibm.com>
2023-05-10 14:00:51 -0400

    Add version and description

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux')
 {
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/pkg-configport.git'), string(name: 'PORT_DESCRIPTION', value: 'package config' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/pkg-configport.git'), string(name: 'PORT_DESCRIPTION', value: 'pkg-config is a computer program that defines and supports a unified interface for querying installed libraries for the purpose of compiling software that depends on them. It allows programmers and installation scripts to work without explicit knowledge of detailed library path information.' )]
   }
 }
