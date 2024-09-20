d34c5d1d74cde5e89040b43866b8db731e6ca124
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:35 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/pkgconfigport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/pkgconfigport.git']]])
         }
   
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/pkgconfigport.git'), string(name: 'PORT_DESCRIPTION', value: 'pkg-config is a computer program that defines and supports a unified interface for querying installed libraries for the purpose of compiling software that depends on them. It allows programmers and installation scripts to work without explicit knowledge of detailed library path information.' ), string(name: 'NODE_LABEL', value: "v2r4")]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/pkgconfigport.git'), string(name: 'PORT_DESCRIPTION', value: 'pkg-config is a computer program that defines and supports a unified interface for querying installed libraries for the purpose of compiling software that depends on them. It allows programmers and installation scripts to work without explicit knowledge of detailed library path information.' ), string(name: 'NODE_LABEL', value: "v2r4")]
   }
 }
